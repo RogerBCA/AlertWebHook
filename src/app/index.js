0b8e18d1f217084e4eee0a40421f0daa4330ab9f
Roger Cruz <rbaltazarc@gmail.com>
2024-10-20 01:42:05 -0500

    prueba de alerta

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
+var io = require("socket.io")(http);
 const app = express();
 
-app.use(express.json());
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.post("/", (req, res) => {
+app.use(express.json());
+app.use("/static", express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/html/index.html");
+});
+
+app.post("/1", (req, res) => {
   console.log(req.body);
+  io.emit("reproducir", { count: 1 });
   res.send("Recibido");
 });
 
-app.listen(3000, () => {
+app.post("/2", (req, res) => {
+  console.log(req.body);
+  io.emit("reproducir", { count: 1 });
+  res.send("Recibido");
+});
+
+http.listen(3000, () => {
   console.log("Servidor escuchando en puerto 3000");
 });
